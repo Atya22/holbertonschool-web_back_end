353b6df24227ab6264cda4f5884947bfd1633bb1
Atya22 <aytaj2002eva@gmail.com>
2024-07-21 13:36:17 +0400

    Update

---
+++ 10-loops.js
@@ -0,0 +1,8 @@
+export default (array, appendString) => {
+    for (const value of array) {
+      const index = array.indexOf(value);
+      array[index] = appendString + value;
+    }
+  
+    return array;
+  };
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# holbertonschool-web_back_end
+ES6 Basics
