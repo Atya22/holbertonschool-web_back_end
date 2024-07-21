a96d79fcab377620045fc69119259a7a895d4f2f
Atya22 <aytaj2002eva@gmail.com>
2024-07-21 16:24:14 +0400

    Delete whitespaces

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,43 +1,43 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    his._name = HolbertonCourse.validateString(name, 'Name');
+    this._name = HolbertonCourse.validateString(name, 'Name');
     this._length = HolbertonCourse.validateNumber(length, 'Length');
     this._students = HolbertonCourse.validateArray(students, 'Students');
   }
   get name() {
     return this._name;
   }
-set name(newName) {
+  set name(newName) {
     this._name = HolbertonCourse.validateString(newName, 'Name');
-}
-get length() {
+  }
+  get length() {
     return this._length;
-}
-set length(newLength) {
+  }
+  set length(newLength) {
     this._length = HolbertonCourse.validateNumber(newLength, 'Length');
-}
-get students() {
+  }
+  get students() {
     return this._students;
-}
-set students(newStudents) {
+  }
+  set students(newStudents) {
     this._students = HolbertonCourse.validateArray(newStudents, 'Students');
-}
-static validateString(value, propertyName) {
+  }
+  static validateString(value, propertyName) {
     if (typeof value !== 'string') {
     throw new TypeError(`${propertyName} Must be a string`);
     }
     return value;
-}
-static validateNumber(value, propertyName) {
+  }
+  static validateNumber(value, propertyName) {
     if (typeof value !== 'number') {
     throw new TypeError(`${propertyName} Must be a number`);
     }
     return value;
-}
-static validateArray(value, propertyName) {
+  }
+  static validateArray(value, propertyName) {
     if (!Array.isArray(value)) {
     throw new TypeError(`${propertyName} Must be an array`);
     }
     return value;
-}
+  }
 }
