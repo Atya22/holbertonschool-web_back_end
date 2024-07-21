3013e941d1f8ac9de6be33dee40a409080bc540b
Atya22 <aytaj2002eva@gmail.com>
2024-07-21 16:25:57 +0400

    Delete whitespaces

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,36 +4,45 @@ export default class HolbertonCourse {
     this._length = HolbertonCourse.validateNumber(length, 'Length');
     this._students = HolbertonCourse.validateArray(students, 'Students');
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     this._name = HolbertonCourse.validateString(newName, 'Name');
   }
+
   get length() {
     return this._length;
   }
+
   set length(newLength) {
     this._length = HolbertonCourse.validateNumber(newLength, 'Length');
   }
+
   get students() {
     return this._students;
   }
+
   set students(newStudents) {
     this._students = HolbertonCourse.validateArray(newStudents, 'Students');
   }
+
   static validateString(value, propertyName) {
     if (typeof value !== 'string') {
     throw new TypeError(`${propertyName} Must be a string`);
     }
     return value;
   }
+
   static validateNumber(value, propertyName) {
     if (typeof value !== 'number') {
     throw new TypeError(`${propertyName} Must be a number`);
     }
     return value;
   }
+
   static validateArray(value, propertyName) {
     if (!Array.isArray(value)) {
     throw new TypeError(`${propertyName} Must be an array`);
