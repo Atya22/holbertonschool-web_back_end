4d1a07ea3480336ee4bbce54f83f90766f799445
Atya22 <aytaj2002eva@gmail.com>
2024-07-21 17:11:17 +0400

    Fix error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
   constructor(sqft) {
-  if (this.constructor !== Building && !this.evacuationWarningMessage) {
-    throw Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-  this._sqft = sqft;
-}
+    this._sqft = sqft;
+  }
 
   get sqft() {
     return this._sqft;
