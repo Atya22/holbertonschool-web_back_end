a45f5739e735c35fe51000c8aa3b365f2d25780b
Atya22 <aytaj2002eva@gmail.com>
2024-07-24 17:09:26 +0400

    Add spaces

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-    const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
+      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
       if (foundGrade) {
         return { ...student, grade: foundGrade.grade };
       }
-    return { ...student, grade: 'N/A' };
+      return { ...student, grade: 'N/A' };
     });
 }
