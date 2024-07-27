6ef0b6936cab55ca3e0d90ab047442ddcddf8238
Atya22 <aytaj2002eva@gmail.com>
2024-07-28 01:14:57 +0400

    Add function to handle API responses

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -3,5 +3,5 @@ export default function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
   })
-  .catch(() => new Error());
+    .catch(() => new Error());
 }
