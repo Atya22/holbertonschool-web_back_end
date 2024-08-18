b249a5eda15745abd84681ebf36c2b722d6da1c7
Atya22 <aytaj2002eva@gmail.com>
2024-08-18 22:08:56 +0400

    Tasks

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 """Module for a function that returns an asyncio.Task.
-
-This module defines a function `task_wait_random` that creates and returns 
-an asyncio.Task for the `wait_random` coroutine with a specified maximum delay.
 """
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
