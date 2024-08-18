50fd304aff250cfc97d2eaee7a8792610c138585
Atya22 <aytaj2002eva@gmail.com>
2024-08-18 19:57:19 +0400

    Correct the error

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union [int, float]]) -> float:
     """
     Returns the sum of a list of mixed floats and ints.
 
