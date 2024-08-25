2e4421551bd6b6b0a5ed973224193191a8c7b34c
Atya22 <aytaj2002eva@gmail.com>
2024-08-25 23:56:05 +0400

    Update

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 """Module for logging stats"""
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 def log_stats():
-    """Function that retrieves logs stats from MongoDB.
-    """
-    # Connect to MongoDB
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.logs
-    collection = db.nginx
+    """Function that retrieves logs stats from MongoDB."""
+    try:
+        # Connect to MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.logs
+        collection = db.nginx
 
-    # Count total logs
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+        # Count total logs
+        total_logs = collection.count_documents({})
+        print(f"{total_logs} logs")
 
-    # Count methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        # Count methods
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        print("Methods:")
+        for method in methods:
+            count = collection.count_documents({"method": method})
+            print(f"\tmethod {method}: {count}")
 
-    # Count specific query
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+        # Count specific query
+        status_check = collection.count_documents({"method": "GET", "path": "/status"})
+        print(f"{status_check} status check")
+
+    except errors.ServerSelectionTimeoutError as err:
+        print(f"Could not connect to MongoDB: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     log_stats()
