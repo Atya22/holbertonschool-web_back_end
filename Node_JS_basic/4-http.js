745b398ce24f3e4bf7379d62347e1203a9d77bde
Atya22 <aytaj2002eva@gmail.com>
2024-09-22 22:52:10 +0400

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+app.listen(1245, () => {
+  console.log('...');
+});
+
+module.exports = app;
